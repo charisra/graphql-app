591f4e0f9095f2740950532de69f78cfaf96608e
Charis <bob@peershipapp.com>
2019-09-02 17:08:53 +0300

    fix format cont

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import Countries from './components/Countries';
+import React from "react";
+import "./App.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import Countries from "./components/Countries";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './components/Home';
-import SingleCountry from './components/SingleCountry';
+import Home from "./components/Home";
+import SingleCountry from "./components/SingleCountry";
 
 const client = new ApolloClient({
-  uri: 'https://countries.trevorblades.com'
+  uri: "https://countries.trevorblades.com"
 });
 
 function App() {
   return (
-      <ApolloProvider client={client}>
-        <Router>
+    <ApolloProvider client={client}>
+      <Router>
         <div className="container">
           <Route exact path="/" component={Home} />
           <Route exact path="/countries/" component={Countries} />
-          <Route path={`/countries/:id`} component={SingleCountry}/>
+          <Route path={`/countries/:id`} component={SingleCountry} />
         </div>
-        </Router>
-      </ApolloProvider>
+      </Router>
+    </ApolloProvider>
   );
 }
 
